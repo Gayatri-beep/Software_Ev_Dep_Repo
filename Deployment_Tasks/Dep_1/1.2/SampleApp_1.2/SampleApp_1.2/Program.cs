39855af4fcbc7ada360e2371f6c2214005bf2c44
Gayatri-beep <104546498@student.swin.edu.au>
2026-08-25 16:19:49 +1000

    Moved all Assignment Tasks to Git Repo

---
+++ Deployment_Tasks/Dep_1/1.2/SampleApp_1.2/SampleApp_1.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleApp_1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, what is your name?");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Hello, " + userName + "!");
+            Console.WriteLine("Thank you for interacting. Program made by Gayatri Sivasamboo S Jeyakumar, for Task 1.2.");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Deployment_Tasks/Dep_1/Software_Deployment_1/Software_Deployment_1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SampleApp
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Gayatri Sivasamboo S Jeyakymar - Deployment Activity 1: Pass Task Completed!");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
